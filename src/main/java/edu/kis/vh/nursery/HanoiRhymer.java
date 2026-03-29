5ddd4d5a918c4f41969967a57829c27bd5938201
Molex3221 <255661@edu.p.lodz.pl>
2026-03-29 20:33:00 +0200

    Usunięcie nieużywanych setterów

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,9 +4,7 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
-    public int reportRejected() {
-        return totalRejected;
-    }
+
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
